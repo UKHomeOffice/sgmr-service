63707f1f8c6b32a11fb3e891fdadf24693da52f6
Pedro Miguel <pedro.miguel@live.co.uk>
2020-09-17 21:35:29 +0100

    Update API URL value to be used in development

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 const apiVersion = 'v1';
-const apiBaseUrl = process.env.SGMR_DATA_API_BASE_URL || 'http://localhost:5000';
+const apiBaseUrl = process.env.SGMR_DATA_API_BASE_URL || 'http://0.0.0.0:5000';
 
 // disabling linter as we cannot export default here (it doesn't work!)
 // eslint-disable-next-line import/prefer-default-export
